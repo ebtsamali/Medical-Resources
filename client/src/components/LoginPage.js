0a011e3ba7e55b64fa97a6ecc213d038818bc945
Rehab Ayman <rehabayman.fcis@gmail.com>
2020-06-22 14:38:50 +0200

    Public Home

--- client/src/components/LoginPage.js
+++ client/src/components/LoginPage.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext, useEffect } from "react";
-import '../styles/login.scss'
+import '../styles/public_home.scss'
 import { AuthContext } from "../providers/auth_provider";
 import ErrorMessage from "./other/ErrorMessage";
 import { Link } from 'react-router-dom';
@@ -11,11 +11,11 @@ const LoginPage = (props) => {
     const { setTitle } = useContext(AppContext);
     const [emailInput, setEmailInput] = useState('')
     const [passwordInput, setPasswordInput] = useState('')
-    // const { message, successful } = props;
 
     useEffect(() => {
         setTitle('Medical Resources::Login')
-    }, [])
+    }, []);
+
     const handleEmailChange = (e) => {
         const { target: { value } } = e;
         setEmailInput(value);
@@ -33,30 +33,46 @@ const LoginPage = (props) => {
         setEmailInput("")
     }
 
-    return (<div className="x-container">
-        <div className="login-card">
-            <h3>Login</h3>
-            <input className="email-input" placeholder="Email" type="text" value={emailInput} onChange={handleEmailChange} />
-            <input className="password-input" placeholder="Password" type="password" value={passwordInput} onChange={handlePasswordChange} />
-            {error && <ErrorMessage message={error} />}
-            <button className="login-btn" onClick={handleLoginClick}>LOGIN</button>
-            <p>Or - <Link to="/register">Create New Account</Link></p>
-            {registerMessage && (
-                <div className="form-group">
-                    <div
-                        className={
-                            successfulRegister
-                                ? "alert alert-success"
-                                : "alert alert-danger"
-                        }
-                        role="alert"
-                    >
-                        {registerMessage}
-                    </div>
+    return (
+        <div className="main-container">
+            <div className="left-container">
+                <span className="proj-title">
+                    <h1>Medical Resources</h1>
+                    <h5>Where Hospitals, Pharmacies and Users come Together</h5>
+                </span>
+                <span className="separator"></span>
+                <p>
+                    Reserve the Room at the Hospital you select.<br></br>
+                    Reserve and Order the Medicine you want from the Pharmacy you Choose.<br></br>
+                    <b>All in One Place.</b>
+                </p>
+            </div>
+            <div className="right-container">
+                <div className="login-card-mod">
+                    <h3>Login</h3>
+                    <input className="email-input-mod" placeholder="Email" type="text" value={emailInput} onChange={handleEmailChange} />
+                    <input className="password-input-mod" placeholder="Password" type="password" value={passwordInput} onChange={handlePasswordChange} />
+                    {error && <ErrorMessage message={error} />}
+                    <button className="login-btn" onClick={handleLoginClick}>LOGIN</button>
+                    <p>Or - <Link to="/register">Create New Account</Link></p>
+                    {registerMessage && (
+                        <div className="form-group">
+                            <div
+                                className={
+                                    successfulRegister
+                                        ? "alert alert-success"
+                                        : "alert alert-danger"
+                                }
+                                role="alert"
+                            >
+                                {registerMessage}
+                            </div>
+                        </div>
+                    )}
                 </div>
-            )}
+            </div>
         </div>
-    </div>)
+    )
 }
 
 export default LoginPage
