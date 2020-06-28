165394db31929653edeca76572e9db95d144e55e
AhmedAdelFahim <ahmedadelfahim@gmail.com>
2020-06-28 15:50:45 +0200

    fix pharmacy profile bug

--- client/src/components/other/WorkingHours.js
+++ client/src/components/other/WorkingHours.js
@@ -5,7 +5,7 @@ import '../../styles/working_hours.scss'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faArrowRight} from "@fortawesome/free-solid-svg-icons";
 
-const WorkingHours = ({weekDetails, setWeekDetails}) => {
+const WorkingHours = ({weekDetails, setWeekDetails, disabled}) => {
     const userRole = JSON.parse(localStorage.getItem("user")) && JSON.parse(localStorage.getItem("user")).role;
     const renderDays = (startIndex, endIndex) => {
         return weekDetails.map((day, index) => {
@@ -19,13 +19,13 @@ const WorkingHours = ({weekDetails, setWeekDetails}) => {
                         offstyle="dark"
                         style="border"
                         size="sm"
-                        disabled={(userRole === "user" || !userRole) ? true : false}
+                        disabled={(userRole === "user" || disabled) ? true : false}
                         onChange={handleDayStatusChange(index)}
                         width={65}/>
-                    <TimePicker disabled={!day.isOpened || userRole === "user" ? true : false} className="time-picker" start="00:00" end="24:00" step={60}
+                    <TimePicker disabled={!day.isOpened || userRole === "user" || disabled ? true : false} className="time-picker" start="00:00" end="24:00" step={60}
                                 onChange={handleStartTimeChange(index)} value={day.startTime}/>
                     <FontAwesomeIcon icon={faArrowRight}/>
-                    <TimePicker disabled={!day.isOpened || userRole === "user" ? true : false} className="time-picker" start="00:00" end="24:00" step={60}
+                    <TimePicker disabled={!day.isOpened || userRole === "user" || disabled ? true : false} className="time-picker" start="00:00" end="24:00" step={60}
                                 onChange={handleEndTimeChange(index)} value={day.endTime}/>
                 </div>)
             }
